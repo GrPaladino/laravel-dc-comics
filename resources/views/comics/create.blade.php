b3ad8eeddd7cb193f9693c0fd1a1a29c4efc13bd
GrPaladino <paladino.graziano@gmail.com>
2024-03-25 17:26:25 +0100

    Fix: add missing @endsession

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,7 +5,7 @@
 @section('main-content')
 <section>
     <div class="container py-4">
-        <h2>crea il nuovo Comic</h2>
+        <h2>Crea il nuovo Comic</h2>
 
         <form action="{{route("comics.store")}}" method="post" class="row g-3">
             @csrf
@@ -47,6 +47,8 @@
 
     </div>
 </section>
+@endsection
+
 
 @section('css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
