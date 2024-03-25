65e57226f5f56b5719399974374a5de9b59212bb
GrPaladino <paladino.graziano@gmail.com>
2024-03-25 15:10:06 +0100

    Feat: create table, model, controller, seeder and views

---
+++ database/migrations/2024_03_25_134037_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->text('thumb')->nullable();
+            $table->double('price', 8, 2);
+            $table->tinyText('series');
+            $table->date('sale_date')->nullable();
+            $table->string('type', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
