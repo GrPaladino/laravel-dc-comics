8c16dac8f6b2cf5a71c729672e99d905540809bf
GrPaladino <paladino.graziano@gmail.com>
2024-03-25 17:08:05 +0100

    Feat: add comics through seeding comics.php

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,31 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics_data = config('comics');
+
+
+
+        foreach ($comics_data as $comic_data) {
+
+            $comic = new Comic;
+
+            $comic->title = $comic_data["title"];
+            $comic->description = $comic_data['description'];
+            $comic->thumb = $comic_data['thumb'];
+
+            $priseString = substr($comic_data['price'], 1, 5);
+            $priceInt = floatval($priseString);
+            $comic->price = $priceInt;
+
+            $comic->series = $comic_data['series'];
+            $comic->sale_date = $comic_data['sale_date'];
+            $comic->type = $comic_data['type'];
+            $comic->save();
+
+
+        }
+
+
+
     }
 }
