6ac53f5b0662b30429ab034068122393352cbad3
GrPaladino <paladino.graziano@gmail.com>
2024-03-26 16:04:23 +0100

    Feat: reseeded db, add delete function and back to info option on edit page

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -7,7 +7,12 @@
     <div class="container py-4">
         <h2>Modifica {{$comic->title}}</h2>
 
-        <a href="{{route('comics.index')}}" class="btn btn-primary my-3">Torna alla lista</a>
+        <a href="{{route('comics.show', $comic)}}" class="btn btn-primary my-3">Torna alle info</a>
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#comic-{{$comic->id}}">
+            Elimina
+        </button>
 
 
         <form action="{{route("comics.update", $comic)}}" method="post" class="row g-3">
@@ -52,6 +57,35 @@
 
     </div>
 </section>
+@endsection
+
+
+@section('modal')
+
+<!-- Modal -->
+<div class="modal fade" id="comic-{{$comic->id}}" tabindex="-1" aria-labelledby="comic-{{$comic->id}}" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminare {{$comic->title}}?</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                L'azione é irreversibile.
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                <form action="{{route('comics.destroy', $comic)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger">Elimina</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 
